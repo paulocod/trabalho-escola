7d0189ea949be4e0f8fd1266282c7ca3078aaa97
paulo campos <paulo.campos.dev@gmail.com>
2023-03-15 21:26:27 -0300

    terminando de configurar o redis e nodejs

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -1,4 +1,8 @@
 import { createClient } from 'redis'
-const redisClient = createClient()
+const url = process.env.REDIS_URL ?? 'redis://localhost:6379'
+
+const redisClient = createClient({
+  url
+})
 
 export { redisClient }
